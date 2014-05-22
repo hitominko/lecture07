d99cf02889260723491f4e4472a520fc32171adf
hitominko <s13144hu@zmac105.sfc.keio.ac.jp>
2014-05-22 12:56:15 +0900

    課題その５

--- js/stats.js
+++ js/stats.js
@@ -2,6 +2,7 @@ var DATA_SIZE = 8;
 
 var inputElements = [];
 var outputMean = null;
+var outputMax = null;
 
 
 /**
@@ -46,17 +47,55 @@ var calcMean = function(){
     }
 };
 
+/*最大値を計算*/
+
+var calcMax = function(){
+    var index = 0;
+    var sum = 0;
+    var n = 0;
+
+    while(index < inputElements.length){
+        var input = inputElements[index];
+        var number = Number(input.value);
+        if(!Number.isNaN(number)){
+            if(sum<number){
+                sum = number;
+                n = n + 1;
+            }
+        }else{
+             alert("計算できません");
+        }
+        index = index + 1;
+    }
+    if(sum == 0){
+        alert("合計が0になっています。");
+    }
+        outputMax.textContent = sum;
+};
+
 var calcStats = function(){
     calcMean();
 };
+
+var about = function(){
+    calcMax(); //calcMax
+};
+
 
 var initApp = function(){
     initInputElements();
 
     outputMean = document.querySelector("#mean");
+    outputMax = document.querySelector("#max");
 
     var calcMeanButton = document.querySelector("#start");
     calcMeanButton.addEventListener("click", calcStats);
+
+    var calcmaxButton = document.querySelector("#calcmax");
+    calcmaxButton.addEventListener("click", about);
+
+
+
 };
 
 initApp();
